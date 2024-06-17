2144ca613f4895c07d70749118bde858f56b2747
lordIguthu <2301942@students.kcau.ac.ke>
2024-06-17 08:39:49 +0300

    Add files via upload

---
+++ DataTypesAndVariables.java
@@ -0,0 +1,56 @@
+public class DataTypesAndVariables {
+
+  // Temperature in degree celsius
+  double temperatureCelsius;
+
+  // Average grade point 
+  double averageGradePoint;
+
+  // Weight in pounds
+  int weightPounds;
+
+  // Height in metres
+  double heightMetres;
+
+  // Miles travelled 
+  double milesTravelled;
+
+  // Students marks 
+  int studentMarks;
+
+  // Student population
+  int studentPopulation;
+
+  // Person's age
+  int personAge;
+
+  // No of days in the past month 
+  int daysInPastMonth;
+
+  // Locker number
+  int lockerNumber;
+
+  // Seconds left in the game 
+  int secondsLeftInGame;
+
+  // No of group members
+  int numberOfGroupMembers;
+
+  // Name of a person
+  String personName;
+
+  // Name of a course 
+  String courseName;
+
+  // Year of study 
+  int yearOfStudy;
+
+  // Phone Number 
+  String phoneNumber;
+
+  // Date of Birth
+  String dateOfBirth;
+
+  // Status to show if a person is alive or dead
+  boolean isAlive;
+}
